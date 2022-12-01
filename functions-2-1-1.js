b7195ed47778ed679c7f426286a0e8e6e43a2e27
Bailey <baileyrosefern@gmail.com>
2022-12-01 15:41:12 -0700

    Question 3 answered

--- functions-2-1-1.js
+++ functions-2-1-1.js
@@ -18,25 +18,38 @@
 // /*
 // 2. Define a function maxOfThree that takes three numbers as arguments and returns the largest of them.
 // */
-maxOfThree = (num1, num2, num3) => {
-  if(num1 > num2 && num1 > num3) {
-    return num1
-  } else if (num2 > num1 && num2 > num3) {
-    return num2
-  } else if (num3 > num1 && num3 > num1) {
-    return num3
-  } else (num1 === num2 || num1 === num3 || num2 === num3) 
-     console.log(`Two of your numbers are the same! Please choose different numbers`)
+// maxOfThree = (num1, num2, num3) => {
+//   if(num1 > num2 && num1 > num3) {
+//     return num1
+//   } else if (num2 > num1 && num2 > num3) {
+//     return num2
+//   } else if (num3 > num1 && num3 > num1) {
+//     return num3
+//   } else (num1 === num2 || num1 === num3 || num2 === num3) 
+//      console.log(`Two of your numbers are the same! Please choose different numbers`)
   
-};
-console.log(maxOfThree(32, 71, 18))
+// };
+// console.log(maxOfThree(32, 71, 18))
 
 // /*
 // 3. Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // */
-// isCharacterAVowel = () => {
-//   // ADD YOUR CODE HERE
-// };
+
+
+//For this one, I looked at Total recall to try and figure out why my old code didn't work. I figured out I had forgotten to write it in a function. Whoops!
+
+let vowels = ["a", "e", "i", "o", "u"];
+isCharacterAVowel = (letter) => {
+let input = letter.toLowerCase();
+if(vowels.includes(input) === true) {
+  return true
+} else {
+  return false
+}
+}
+
+console.log(isCharacterAVowel("f"))
+
 
 // /*
 // 4. Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and multiplyArray([1,2,3,4]) should return 24.
